668cbd72a1882d8d71cb09b41e3753d21b418327
Nuno <nmot97@gmail.com>
2022-03-23 23:06:12 +0000

    Update dashboard_beta.R

--- dashboard_beta.R
+++ dashboard_beta.R
@@ -16,12 +16,25 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "home",
-            h5("Welcome to the home page"),
+            h4("Welcome to the home page"),
             box(
-              title = "Basic MIMIC-III statistics", width = 15, solidHeader = TRUE,
+              title = "Basic MIMIC-III statistics between 2001-2012", width = 8, solidHeader = TRUE,
+              #colocar dia e mes
               
-              HTML('Hello <')
+              HTML('<b> Number of distinct ICU stays:</b> 53 423 <br>'),
+              HTML('<b> Number of hospital admissions:</b> 49 785 <br>'),
+              HTML('<b> Number of distinct patients:</b> 38 597 <br>'),
+              HTML('<b> Gender, Male %:</b> 55.9% <br>'),
+              HTML('<b> Average age, years:</b> 65.8'),
+              HTML('<b> ICU length of stay, average days:</b> 2.1 <br>'),
+              HTML('<b> Hospital length of stay, average days:</b> 6.9 <br>'),
+              HTML('<b> ICU Mortality, %:</b> 8.5 <br>'),
+              HTML('<b> Hospital mortality,% :</b> 11.5 <br>')
             ),
+            
+          
+            
+
     ),
     
     
@@ -41,3 +54,4 @@ ui <- dashboardPage(header, sidebar, body)
 server <- function(input, output) { }
 
 shinyApp(ui, server)
+
